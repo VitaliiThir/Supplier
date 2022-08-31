8689570692f3244a3b7a0c21a71b6aaa4db9c4e0
VitaliiThir <Vitslikus865@gmail.com>
2022-08-31 23:33:52 +0400

    ggg

--- app/StoreProducts.php
+++ app/StoreProducts.php
@@ -7,12 +7,11 @@ use Bitrix\Catalog\StoreProductTable;
 use Bitrix\Iblock\ElementTable;
 use Bitrix\Main\ArgumentException;
 use Bitrix\Main\Diag\Debug;
-use Bitrix\Main\Loader;
+use Bitrix\Main\LoaderException;
 use Bitrix\Main\ObjectPropertyException;
 use Bitrix\Main\SystemException;
 use CIBlockElement;
 use Exception;
-use Bitrix\Main\LoaderException;
 
 class StoreProducts extends Parser
 {
@@ -32,33 +31,15 @@ class StoreProducts extends Parser
     private array $final_products_arr;
 
     /**
+     * @throws ArgumentException
      * @throws ObjectPropertyException
      * @throws SystemException
-     * @throws ArgumentException
      */
     public function __construct()
     {
         $this->store_products = $this->get_products_arr();
         $this->catalog_filtered_products = $this->get_filtered_catalog();
         $this->final_products_arr = $this->get_final_products_arr();
-    }
-
-    /**
-     * @return array
-     */
-    private function get_final_products_arr(): array
-    {
-        $final_arr = [];
-
-        foreach ($this->store_products as $store_id => $store) {
-            foreach ($store as $art => $prod) {
-                $prod['PRODUCT_ID'] = $this->catalog_filtered_products[$art]['ID'];
-                $prod['STORE_ID'] = $store_id;
-                $final_arr[] = $prod;
-            }
-        }
-
-        return $final_arr;
     }
 
     /**
@@ -73,7 +54,7 @@ class StoreProducts extends Parser
 
             $dbItems = ElementTable::getList(array(
                 'select' => array('ID', 'IBLOCK_ID'),
-                'filter' => array('IBLOCK_ID' => $this->catalog_ib_id)
+                'filter' => array('IBLOCK_ID' => (int)$this->catalog_ib_id)
             ));
 
             while ($arItem = $dbItems->fetch()) {
@@ -102,33 +83,54 @@ class StoreProducts extends Parser
     }
 
     /**
+     * @return array
+     */
+    public function get_final_products_arr(): array
+    {
+        $final_arr = [];
+
+        foreach ($this->store_products as $store_id => $store) {
+            foreach ($store as $art => $prod) {
+                $prod['PRODUCT_ID'] = $this->catalog_filtered_products[$art]['ID'];
+                $prod['STORE_ID'] = $store_id;
+                $final_arr[] = $prod;
+            }
+        }
+
+        return $final_arr;
+    }
+
+    /**
+     * Обновление остатков
+     * @throws ArgumentException
      * @throws ObjectPropertyException
      * @throws SystemException
-     * @throws ArgumentException
      * @throws Exception
-     * Временный код обновления остатков для дальнейшего внедрения
      */
     public function store_products_update()
     {
         try {
-            Loader::includeModule('iblock');
-
             foreach ($this->final_products_arr as $product) {
+                $product_id = (int) $product['PRODUCT_ID'];
+                $store_id = (int) $product['STORE_ID'];
+                $amount = (int) $product[$this->catalog_quantity];
 
                 $rsStoreProduct = StoreProductTable::getList(array(
-                    'filter' => array('=PRODUCT_ID' => $product['ID'], 'STORE.ACTIVE' => 'Y'),
-                ))->fetch();
+                    'filter' => array('=PRODUCT_ID' => $product_id, 'STORE.ACTIVE' => 'Y', '=STORE_ID'=>$store_id),
+                ));
 
-                $updateStore = StoreProductTable::update(
-                    $rsStoreProduct['ID'],
-                    array(
-                        'PRODUCT_ID' => intval($product['ID']),
-                        'STORE_ID' => $product['STORE_ID'],
-                        'AMOUNT' => $product['CATALOG_QUANTITY']
-                    )
-                );
+                if ($arStoreProduct = $rsStoreProduct->fetch()) {
+                    $updateStore = StoreProductTable::update(
+                        $arStoreProduct['ID'],
+                        array(
+                            'PRODUCT_ID' => $product_id,
+                            'STORE_ID' => $store_id,
+                            'AMOUNT' => $amount
+                        )
+                    );
+                }
 
-                $updateQuantity = ProductTable::update($product['ID'], array('QUANTITY' => $product['CATALOG_QUANTITY']));
+                $updateQuantity = ProductTable::update($product_id, array('QUANTITY' => $amount));
             }
         } catch (LoaderException $e) {
             Debug::dump($e->getMessage());
--- test.php
+++ test.php
@@ -1,14 +1,15 @@
 <?php
 
-use Bitrix\Main\Diag\Debug;
-use Bitrix\Main\LoaderException;
+use Bitrix\Main\Loader;
 use Supplier\StoreProducts;
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 require('vendor/autoload.php');
 
-$store = new StoreProducts();
+Loader::includeModule('catalog');
 
+$store = new StoreProducts();
 $store->store_products_update();
+
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
