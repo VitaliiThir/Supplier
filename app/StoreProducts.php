4b09d31926067329bda945c765da02160da79b09
VitaliiThir <Vitslikus865@gmail.com>
2022-09-01 00:05:41 +0400

    fff

--- app/StoreProducts.php
+++ app/StoreProducts.php
@@ -110,13 +110,15 @@ class StoreProducts extends Parser
     public function store_products_update()
     {
         try {
+            $totals = [];
+
             foreach ($this->final_products_arr as $product) {
-                $product_id = (int) $product['PRODUCT_ID'];
-                $store_id = (int) $product['STORE_ID'];
-                $amount = (int) $product[$this->catalog_quantity];
+                $product_id = (int)$product['PRODUCT_ID'];
+                $store_id = (int)$product['STORE_ID'];
+                $amount = (int)$product[$this->catalog_quantity];
 
                 $rsStoreProduct = StoreProductTable::getList(array(
-                    'filter' => array('=PRODUCT_ID' => $product_id, 'STORE.ACTIVE' => 'Y'),
+                    'filter' => array('=PRODUCT_ID' => $product_id, 'STORE.ACTIVE' => 'Y', '=STORE_ID' => $store_id),
                 ));
 
                 if ($arStoreProduct = $rsStoreProduct->fetch()) {
@@ -124,14 +126,21 @@ class StoreProducts extends Parser
                         $arStoreProduct['ID'],
                         array(
                             'PRODUCT_ID' => $product_id,
-                            'STORE_ID' => $store_id,
                             'AMOUNT' => $amount
                         )
                     );
+                } else {
+                    $addStore = \Bitrix\Catalog\StoreProductTable::add(array('PRODUCT_ID' => $product_id, 'STORE_ID' => $store_id, 'AMOUNT' => $amount));
                 }
 
-                $updateQuantity = ProductTable::update($product_id, array('QUANTITY' => $amount));
+                $totals[$product_id] += $amount;
+
             }
+
+            foreach ($totals as $product_id => $quantity) {
+                $updateQuantity = ProductTable::update($product_id, array('QUANTITY' => $quantity));
+            }
+
         } catch (LoaderException $e) {
             Debug::dump($e->getMessage());
         }
