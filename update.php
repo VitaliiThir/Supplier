4d82669ae374bd316c9c1de8cc82b3f932fd0c8e
VitaliiThir <Vitslikus865@gmail.com>
2022-09-01 14:02:45 +0400

    last

--- update.php
+++ update.php
@@ -3,7 +3,7 @@
 use Bitrix\Main\Loader;
 use Supplier\StoreProducts;
 
-$_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__)."/../"); // "c:/openserver/domains/bitrix.test/";
+$_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__)."/../");
 $DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
 
 define("NO_KEEP_STATISTIC", true);
